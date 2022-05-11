b8c91bf140841d747f0b63bff32f1d975cfe7ade
AhmedSoli92 <ahmed.soli101@gmail.com>
2022-05-11 03:58:52 +0200

    frontend: header

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
-
+import './header.css'
 export default function Header() {
   return (
-    <div>Header</div>
+    <div className='header'>
+        <div className="header-title">
+            <span className="main-header">Blog</span>
+            <span className="secondary-header">Developer Blog</span>
+        </div>
+        <img src="/assets/images/header blog.jpg" alt="" className="img-header" />
+    </div>
   )
 }
