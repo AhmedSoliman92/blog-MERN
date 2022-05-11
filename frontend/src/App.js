eab7e01fefabef46915aecffe03805699960d4f7
AhmedSoli92 <ahmed.soli101@gmail.com>
2022-05-11 23:18:41 +0200

    frontend: create new blog

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import Bar from './components/bar/Bar'
 import Home from './pages/home/Home'
-import SinglePost from  './pages/singlePost/SinglePost.jsx'
+import SinglePost from  './pages/singlePost/SinglePost'
+import CreatePost  from "./pages/createPost/CreatePost";
 function App() {
   return (
     <>
       <Bar/>
-      <SinglePost/>
+      <CreatePost/>
     </>
   );
 }
---
+++ frontend/src/pages/createPost/CreatePost.jsx
@@ -0,0 +1,32 @@
+import './createPost.css'
+import { useState } from "react";
+
+const CreatePost = () => {
+    const [files, setFiles]= useState([]);
+    const handlePreview = (e)=>{
+        setFiles(e.target.files);
+    }
+    
+    return (
+        <div className="create-post-container">
+            
+            {files.length===1 && <img src={URL.createObjectURL(files[0])} alt="" className="uploaded-img" />}
+            <form className="post-form">
+                <div className="top-form">
+                <button className="submit-btn" type="submit">Publish</button>
+                <label htmlFor="file" className="file-label">
+                    <i className="file-icon fa-solid fa-plus"></i>
+                </label>
+                <input id="file" type="file" style={{display:'none'}} onChange={handlePreview} />
+                
+                <input type="text" className="post-title" placeholder="Title..." autoFocus={true} />
+                </div>
+                <textarea name="" id="" cols="30" rows="15" className="desc-post" placeholder="Write your description..." ></textarea>
+                
+            </form>
+            
+        </div>
+    );
+}
+
+export default CreatePost;
