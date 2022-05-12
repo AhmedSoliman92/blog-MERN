b0d91cd15d7928b255e6784ca636ec35bf1cda7d
AhmedSoli92 <ahmed.soli101@gmail.com>
2022-05-12 02:44:48 +0200

    frontend: Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,12 +5,23 @@ import CreatePost  from "./pages/createPost/CreatePost";
 import UpdateAccount from "./pages/updateAccount/UpdateAccount";
 import Login from '../src/pages/login/Login'
 import Register from '../src/pages/register/Register'
+import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
 function App() {
   return (
-    <>
-      <Bar/>
-      <Register/>
-    </>
+      <Router>
+        <Bar/>
+          <Routes>
+            <Route exact path='/' element={<Home/>}/>
+            <Route path='/register' element={<Register/>}/>
+            <Route path='/login' element={<Login/>}/>
+            <Route path='/update' element={<UpdateAccount/>}/>
+            <Route path='/create' element={<CreatePost/>}/>
+            <Route path='/:id' element={<SinglePost/>}/>
+          </Routes>
+        
+      </Router>
+    
+      
   );
 }
 
